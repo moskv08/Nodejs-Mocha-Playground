dec7d12e0aec56e75c2d69d11589d6af5aae46f9
Fabrizio <Fabrizio_39@hotmail.com>
2019-09-21 19:37:17 +0200

    install mocha

---
+++ test/controllers/authController.spec.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const authController = require('../../controllers/authController');
+
+describe('AuthController', function () {
+    describe('isAuthorized', function () {
+        it('should return false if not authorized', function () {
+
+        });
+    });
+});
