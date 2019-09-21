6c3694ab61f112d43792446978509f3429f68195
Fabrizio <Fabrizio_39@hotmail.com>
2019-09-21 20:10:42 +0200

    test aync function

--- test/controllers/authController.spec.js
+++ test/controllers/authController.spec.js
@@ -4,6 +4,28 @@ const authController = require('../../controllers/authController');
 describe('AuthController', function () {
     describe('isAuthorized', function () {
         it('should return false if not authorized', function () {
+            const role = 'admin'
+            const roles = ['user'];
+            const expected = authController.isAuthorized(roles, role);
+            assert.equal(false, expected);
+        });
+
+        it('should return true if authorized', function () {
+            const role = 'admin'
+            const roles = ['user', 'admin'];
+            const expected = authController.isAuthorized(roles, role);
+            assert.equal(true, expected);
+        });
+    });
+
+    describe('isAuthorizedAsync', function () {
+        it('should return false if not authorized', function (done) {
+            const role = 'admin'
+            const roles = ['user'];
+            authController.isAuthorizedAsync(roles, role, function (isAuth) {
+                assert.equal(false, isAuth);
+                done();
+            });
 
         });
     });
