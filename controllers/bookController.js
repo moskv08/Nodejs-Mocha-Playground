fe0d4e123010550f9001e0905a262aa5aa54c138
Fabrizio <Fabrizio_39@hotmail.com>
2019-09-27 18:20:43 +0200

    extended functionality to handle auth

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,7 +1,17 @@
+
+
 function BookController() {
 
     function getIndex(req, res) {
-        res.render('index');
+        try {
+            if (req.user.isAuthorized('admin')) {
+                return res.render('profile');
+            }
+            res.render('index');
+        } catch (error) {
+            res.render('error');
+        }
+
     }
     return { getIndex }
 }
