cfe9b2a4dac20a59559d77b00c19103a86cf8fbd
Fabrizio <Fabrizio_39@hotmail.com>
2019-09-27 18:21:22 +0200

    created a stub for user.isAuthorized function

--- test/controllers/bookController.spec.js
+++ test/controllers/bookController.spec.js
@@ -15,16 +15,53 @@ before(() => {
 
 describe('BookController', function () {
     describe('getIndex', function () {
-        it('should render index once', function () {
 
-            const req = {};
-            const res = {
-                render: sinon.spy()
-            };
+        let user = {};
+        beforeEach(function () {
+            user = {
+                roles: ['user'],
+                isAuthorized: function (neededRole) {
+                    return this.roles.indexOf(neededRole) >= 0;
+                }
+            }
+        });
+
+        it('should render profile once if user is authorized', function () {
+            // Stub isAuhtorized function
+            const isAuth = sinon.stub(user, 'isAuthorized').returns(true);
+            const req = { user };
+            const res = { render: sinon.spy() };
 
             bookController.getIndex(req, res);
 
+            isAuth.calledOnce.should.be.true;
+            res.render.firstCall.args[0].should.equal('profile');
+            res.render.calledOnce.should.be.true;
+        });
+
+        it('should render index once if user was not authorized', function () {
+            // Stub isAuhtorized function
+            const isAuth = sinon.stub(user, 'isAuthorized').returns(false);
+            const req = { user };
+            const res = { render: sinon.spy() };
+
+            bookController.getIndex(req, res);
+
+            isAuth.calledOnce.should.be.true;
             res.render.firstCall.args[0].should.equal('index');
+            res.render.calledOnce.should.be.true;
+        });
+
+        it('should throw an error if authorization was not possible', function () {
+            // Stub isAuhtorized function
+            const isAuth = sinon.stub(user, 'isAuthorized').throws();
+            const req = { user };
+            const res = { render: sinon.spy() };
+
+            bookController.getIndex(req, res);
+
+            isAuth.calledOnce.should.be.true;
+            res.render.firstCall.args[0].should.equal('error');
             res.render.calledOnce.should.be.true;
         });
     });
